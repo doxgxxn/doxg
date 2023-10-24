3a8f0bc92c48837d8a354cd0c0a18a74c1a58961
doxg <praze74@gmail.com>
2023-10-24 13:39:56 +0900

    game updated

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,15 +1,14 @@
-# 이제 모듈을 임포트할 수 있습니다
-from doxg.app import imp, pic, who
+import doxg
 
 # 나머지 테스트 코드
 def test_pic():
-    pic()
+    doxg.pic()
     assert True
 
 def test_who():
-    who()
+    doxg.who()
     assert True
 
 def test_imp():
-    imp()
+    doxg.imp()
     assert True
